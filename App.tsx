e7b4f00d04063bc99c83950e55e55fe00c207c8d
haozhen <whaozhen01@gmail.com>
2025-04-17 08:42:12 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
 // App.tsx
 import React from 'react';
 import AppNavigator from './navigator/AppNavigator'; // Adjust path as needed
+import { OnboardingProvider } from './context/OnboardingContext';
+import { ProductProvider } from './context/ProductContext';
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <OnboardingProvider>
+      <ProductProvider>
+        <AppNavigator />
+      </ProductProvider>
+    </OnboardingProvider>
+  );
 }
